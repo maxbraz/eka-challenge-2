426765632f280f3ce59c6a790f92ef6cccb067a7
Max Braz <braz.max@gmail.com>
2017-12-06 11:28:11 -0800

    add route to homepage

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router-dom'
 import {Card, CardActions, CardHeader, CardMedia} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -15,7 +16,7 @@ const style = {
   }
 };
 
-const Profile = () => (
+const Profile = withRouter(({ history }) => (
   <Card>
     <CardHeader
       title="Your Profile is Complete!"
@@ -24,9 +25,9 @@ const Profile = () => (
       <img src="http://i0.kym-cdn.com/photos/images/facebook/000/011/296/success_baby.jpg" />
     </CardMedia>
     <CardActions>
-      <RaisedButton label="Home" primary={true} style={style.button} />
+      <RaisedButton label="Home" primary={true} style={style.button} onClick={() => {history.push('landingPage')}} />
     </CardActions>
   </Card>
-);
+));
 
 export default Profile;
